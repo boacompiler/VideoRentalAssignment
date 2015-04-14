f24c71d368dcba9e4d331921b22fc99397f859a5
Robert Stephens <thegeekiestthingonearth@live.co.uk>
2015-04-14 21:42:59 +0100

    working copy?

--- prototypingAssignment/UserControls/logInControl.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class logInControl
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        'temporary
-
-        Form1.LogIn(Form1.isAdmin)
-
-    End Sub
-End Class
